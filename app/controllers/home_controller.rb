8647cc47d9d554d47cb806e723889168e477dd2d
navcsdev <nguyenanhvu.cs@gmail.com>
2019-12-08 14:58:54 +0700

    Add filter shares for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   skip_before_action :authorized, only: [:index]
 
   def index
-    @movies = Movie.last(10)
+    if logged_in?
+      @movies = Movie.where(sharers_id: current_user.id).last(10)
+    else
+      @movies = Movie.last(10)
+    end
   end
 end
